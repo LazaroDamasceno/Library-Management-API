9835a673287827681ca819123521029e34ba86ce
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-06-10 21:21:52 -0300

    Deleted one query from BookBorrowRepository

--- v1/src/main/java/com/api/v1/book_borrow/BookBorrowRepository.java
+++ v1/src/main/java/com/api/v1/book_borrow/BookBorrowRepository.java
@@ -1,6 +1,5 @@
 package com.api.v1.book_borrow;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -28,19 +27,6 @@ public interface BookBorrowRepository extends JpaRepository<BookBorrow, UUID> {
         WHERE bb.borrower = :borrower
     """)
     List<BookBorrow> findAllBookBorrowsByBorrowers(@Param("borrower") Borrower borrower);
-
-    @Query("""
-        SELECT bb
-        FROM BookBorrow bb
-        WHERE bb.borrower = :borrower
-        AND bb.borrowDateTime >= :firstDateTime
-        AND bb.borrowDateTime <= :lastDateTime
-    """)
-    List<BookBorrow> findBookBorrowsByBorrowersBetweenDateTimes(
-        @Param("borrower") Borrower borrower,
-        @Param("firstDateTime") LocalDateTime firstDateTime,
-        @Param("lastDateTime") LocalDateTime lastDateTime
-    );
 
     @Query("""
         SELECT bb
