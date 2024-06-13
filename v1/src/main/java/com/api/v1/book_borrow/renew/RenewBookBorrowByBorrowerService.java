366a87a08aae47c4a1dd689978bc7a4e9475f344
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-06-13 19:47:50 -0300

    Renoved unused import

--- v1/src/main/java/com/api/v1/book_borrow/renew/RenewBookBorrowByBorrowerService.java
+++ v1/src/main/java/com/api/v1/book_borrow/renew/RenewBookBorrowByBorrowerService.java
@@ -1,7 +1,5 @@
 package com.api.v1.book_borrow.renew;
 
-import jakarta.validation.constraints.NotNull;
-
 public interface RenewBookBorrowByBorrowerService {
 
     void RenewByBorrower(BookBorrowDataDTO dto);
